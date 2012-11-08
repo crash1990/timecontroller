0c085dd08c300d100d399758e8ee197bc5607c60
Mateusz <mwojciechowski1990@gmail.com>
2012-11-09 00:48:46 +0100

    union operator

--- timecontroller/src/main/java/org/jboss/tools/example/richfaces/model/Daily.java
+++ timecontroller/src/main/java/org/jboss/tools/example/richfaces/model/Daily.java
@@ -8,6 +8,7 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.ParameterExpression;
 import javax.persistence.criteria.Root;
 
 import org.jboss.tools.example.richfaces.data.QueryParametersInterface;
@@ -25,11 +26,23 @@ public class Daily extends TimePeriod implements QueryParametersInterface{
 	public void setHour(String hour) {
 		this.hour = hour;
 	}
+	private CriteriaQuery<Daily> q; 
+	private Root<Daily> c;
 	
 	public void queryBuilder(CriteriaBuilder cb){
-		CriteriaQuery<Daily> q = cb.createQuery(Daily.class);
-		Root<Daily> c = q.from(Daily.class);
+		q = cb.createQuery(Daily.class);
+		c = q.from(Daily.class);
 		q.select(c);
+		
+	}
+	public void union( CriteriaBuilder cb, String first, String second ){
+		
+		q.where(
+			      cb.or(
+			          cb.equal(c.get("TIMEDOMAIN_ID"), first),
+			          cb.equal(c.get("TIMEDOMAIN_ID"), second)
+			      )
+			      );
 		
 		
 	}
